18080b45b2ecb39967723837d4af2bac286c698b
nbzeman <nbzeman@gmail.com>
2015-09-17 13:16:12 -0400

    moving code for ghost based on inputs

---
+++ Assets/Scenes/Lesson 2/Z_3rdPersonController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_3rdPersonController : MonoBehaviour {
+
+	public float rotMultY = 1;
+	public float transMultZ = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//this will translate the local Z based on the Vertical axis in the Unity Input Manager
+		transform.Translate (0, 0, Input.GetAxis ("Vertical") * transMultZ, Space.Self);
+
+		//this will rotate the object in the positive or negative Y axis (Euler) based on the Mouse X input from Unity's input manager
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * rotMultY, 0);
+
+
+
+	}
+}
