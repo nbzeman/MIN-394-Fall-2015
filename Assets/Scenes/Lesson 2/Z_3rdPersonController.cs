849ba547b0924cd73ac0fcde42fb40d48cbf8a4c
nbzeman <nbzeman@gmail.com>
2015-09-22 12:51:58 -0400

    added code for floating

--- Assets/Scenes/Lesson 2/Z_3rdPersonController.cs
+++ Assets/Scenes/Lesson 2/Z_3rdPersonController.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class Z_3rdPersonController : MonoBehaviour {
 
+	//sin float controls
+	public float sinTest;
+	public float amp;
+	public float offset;
+	public float freq;
 
+	//input multipliers
 	public float rotMultY = 1;
 	public float transMultZ = 1;
 
@@ -16,14 +22,21 @@ public class Z_3rdPersonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		//creates the sin function to give the wave-like motion to the Y axis of the ghost
+		sinTest =   (  (Mathf.Sin (Time.time * freq)  ) * amp ) + offset;
+		//assign the sinTest value to the Y translate of the ghost position, passes through current values of the x and z
+		transform.position = new Vector3 (transform.position.x , sinTest, transform.position.z);
+
+
 
 		//this will translate the local Z based on the Vertical axis in the Unity Input Manager
 		transform.Translate (0, 0, Input.GetAxis ("Vertical") * transMultZ, Space.Self);
 
-		if (Input.GetMouseButton (1)) {
-			//this will rotate the object in the positive or negative Y axis (Euler) based on the Mouse X input from Unity's input manager
-			transform.Rotate (0, Input.GetAxis ("Mouse X") * rotMultY, 0);
-		}
+
+
+		//this will rotate the object in the positive or negative Y axis (Euler) based on the Mouse X input from Unity's input manager
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotMultY, 0);
+
 
 
 	}
