7d02ce1b7f39e6f601b18dde70c4bd46c1d95959
Nicholas Zeman <nbzeman@gmail.com>
2015-10-06 13:27:32 -0400

    Added Physics Trigger script

---
+++ Assets/Scenes/Lesson 5/Z_OnTrigger_Enter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_OnTrigger_Enter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider thing){
+
+		print (thing.gameObject.name + " collided with " + this.gameObject.name);
+
+		if (thing.gameObject.tag == "Player") {
+
+			print ("TRIGGER ENTERED");
+
+		}
+
+	}
+
+	void OnTriggerStay(Collider thing){
+		
+		print (thing.gameObject.name + " is colliding with " + this.gameObject.name);
+		
+		if (thing.gameObject.tag == "Player") {
+			
+			print ("TRIGGER STAY");
+			
+		}
+		
+	}
+
+	void OnTriggerExit(Collider thing){
+		
+		print (thing.gameObject.name + " is exiting a collision with " + this.gameObject.name);
+		
+		if (thing.gameObject.tag == "Player") {
+			
+			print ("TRIGGER LEAVE");
+			
+		}
+		
+	}
+
+
+}
