12d01e34a8a3bf99d3121bf54a29239c261ec766
Nicholas Zeman <nbzeman@gmail.com>
2015-10-22 12:51:00 -0400

    added examples for float up stuff

---
+++ Assets/Scenes/Lesson 5/Z_Physics_Float_Control.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_Physics_Float_Control : MonoBehaviour {
+
+	Rigidbody rb;
+	public float force = 35;
+	// Use this for initialization
+	void Start () {
+
+		rb = this.gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey ("space")) {
+
+			rb.AddForce(0,force,0);
+		}
+
+
+	}
+}
