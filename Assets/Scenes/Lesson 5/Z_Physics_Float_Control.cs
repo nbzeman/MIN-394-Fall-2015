26f969bca1251675f3a573ad635f15f8e320ea04
Nicholas Zeman <nbzeman@gmail.com>
2015-10-27 12:46:20 -0400

    adding temp directory

--- Assets/Scenes/Lesson 5/Z_Physics_Float_Control.cs
+++ Assets/Scenes/Lesson 5/Z_Physics_Float_Control.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Z_Physics_Float_Control : MonoBehaviour {
 
-	Rigidbody rb;
+	//Rigidbody rb;
 	public float force = 35;
+
+
 	// Use this for initialization
 	void Start () {
 
-		rb = this.gameObject.GetComponent<Rigidbody> ();
+		//rb = this.gameObject.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -16,7 +18,7 @@ public class Z_Physics_Float_Control : MonoBehaviour {
 	
 		if (Input.GetKey ("space")) {
 
-			rb.AddForce(0,force,0);
+			this.gameObject.GetComponent<Rigidbody>().AddForce(0,force,0);
 		}
 
 
