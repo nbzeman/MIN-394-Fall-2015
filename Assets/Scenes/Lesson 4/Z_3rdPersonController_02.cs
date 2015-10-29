3c0c0e67fef506f9a0a72d76cdf1e0e72e38fb38
Nicholas Zeman <nbzeman@gmail.com>
2015-10-29 12:30:55 -0400

    Add prefab for movement

---
+++ Assets/Scenes/Lesson 4/Z_3rdPersonController_02.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_3rdPersonController_02 : MonoBehaviour {
+
+	//sin float controls
+	public float sinTest;
+	public float amp;
+	public float offset;
+	public float freq;
+
+	//input multipliers
+	public float rotMultY = 1;
+	public float rotMultZ = 1;
+
+	public float transMultZ = 1;
+	public float transMultX = 1;
+
+	
+	//external transforms
+	public Transform tilt;
+	public Transform sinFloater;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//creates the sin function to give the wave-like motion to the Y axis of the ghost
+		sinTest =   (  (Mathf.Sin (Time.time * freq)  ) * amp ) + offset;
+		//assign the sinTest value to the Y translate of the ghost position, passes through current values of the x and z
+		sinFloater.transform.position = new Vector3 (transform.position.x , sinTest, transform.position.z);
+
+
+
+		//this will translate the local Z based on the Vertical axis in the Unity Input Manager and the local X based on the horizontal input
+		transform.Translate (Input.GetAxis ("Strafe") * transMultX, 0, Input.GetAxis ("Vertical") * transMultZ, Space.Self);
+
+
+
+		//this will rotate the object in the positive or negative Y axis (Euler) based on the Mouse X input from Unity's input manager
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotMultY, 0);
+
+		//this will provide the tilting effect for the Tilt transform
+		tilt.localEulerAngles = new Vector3(Input.GetAxis ("Rocker") * 21, 0, -Input.GetAxis("Strafe")*20);
+
+
+
+
+
+	}
+}
---
+++ Assets/Scenes/Lesson 4/Z_FloaterControl_02.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_FloaterControl_02 : MonoBehaviour {
+	
+	//sin float controls
+	public float sinTest;
+	public float amp;
+	public float offset;
+	public float freq;
+	
+	//input multipliers
+	public float rotMultY = 1;
+	public float rotMultZ = 1;
+	
+	public float transMultZ = 1;
+	public float transMultX = 1;
+	public float transMultY = 1;
+
+	
+	
+	//external transforms
+	public Transform tilt;
+	public Transform sinFloater;
+	public Transform floaterP;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//creates the sin function to give the wave-like motion to the Y axis of the ghost
+		sinTest =   (  (Mathf.Sin (Time.time * freq)  ) * amp ) + offset;
+		//assign the sinTest value to the Y translate of the ghost position, passes through current values of the x and z
+		sinFloater.transform.localPosition = new Vector3 (transform.position.x , sinTest, -transform.position.z);
+		
+		
+		
+		//this will translate the local Z based on the Vertical axis in the Unity Input Manager and the local X based on the horizontal input
+		transform.Translate (Input.GetAxis ("Strafe") * transMultX, 0, Input.GetAxis ("Vertical") * transMultZ, Space.Self);
+		
+		
+		
+		//this will rotate the object in the positive or negative Y axis (Euler) based on the Mouse X input from Unity's input manager
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotMultY, 0);
+		
+		//this will provide the tilting effect for the Tilt transform
+		tilt.localEulerAngles = new Vector3(Input.GetAxis ("Rocker") * 21, 0, -Input.GetAxis("Strafe")*20);
+		
+		floaterP.position = new Vector3 (0,Input.GetAxis ("FloatY")*transMultY,0);
+		
+		
+		
+	}
+}
